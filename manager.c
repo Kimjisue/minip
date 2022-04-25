f8456da3f3bb5e2e29a88af40bdad9e4ba86828d
Kimjisue <22100194@handong.ac.kr>
2022-04-25 17:09:47 +0900

    data

--- manager.c
+++ manager.c
@@ -33,30 +33,45 @@ int selectDataNo(Product *p, int count){
     getchar();
     return no;
 }
-
-//배열데이터를 파일에 저장하는 함수
 void saveData(Product p[], int count){
-	FILE* fp;
+        FILE* fp;
 
-	//중량 가격 제품명
-	fp= fopen("product.txt","wt");
-	
-	
-	fclose(fp);
-	printf("저장됨!\n");
+        //중량 가격 제품명
+        fp= fopen("product.txt","wt");
+        for(int i=0;i<count;i++){
+        if(p[i].price == -1) continue;
+        fprintf(fp, "%s %d %d\n", p[i].name, p[i].weight, p[i].price);
+    }
+    fclose(fp);
+
+
+        fclose(fp);
+        printf("저장됨!\n");
 }
 
 
 //파일에서 데이터 불러오는 함수
 int loadData(Product *p){
-	int count=0;
-	FILE*fp;
+        int count=0;
+        FILE*fp;
 
-	//파일 내용을 읽어와서 배열에 값 추가하기
+        //파일 내용을 읽어와서 배열에 값 추가하기
+        fp = fopen("product.txt","rt");
+        if(fp == NULL){
+        printf("파일 없음");
+        return 0;
+        }
+        for(count=0;count<100;count++){
+        fscanf(fp,"%s",p[count].name);
+        if(feof(fp)) break;
+        fscanf(fp, "%s %d %d\n", p[count].name, p[count].weight, p[count].price);
+        if(feof(fp)) break;
+        }
+        fclose(fp);
 
 
 
-
-	printf("=> 로딩 성공!\n");
-	return count;
+        printf("=> 로딩 성공!\n");
+        return count;
 }
+
